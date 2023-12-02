039ec25a07bbd9f4cd71f41d3a8c87b10d4b76a6
Samuel Odumu <themainsamuel@gmail.com>
2023-12-02 01:15:48 +0100

    fixed betty style to _printf.c file

--- _printf.c
+++ _printf.c
@@ -1,12 +1,15 @@
 #include "main.h"
 
-/*
-* _printf - a function that produces output according to a format.
-* Prototype: int _printf(const char *format, ...);
-* @format: a string of characters.
-* Returns: the number of characters printed
-* (excluding the null byte used to end output to strings)
-*/
+/**
+ * _printf - a function that produces output according to a format.
+ * Prototype: int _printf(const char *format, ...);
+ *
+ * @format: a string of characters.
+ *
+ * Return: the number of characters printed
+ * (excluding the null byte used to end output to strings)
+ *
+ */
 
 int _printf(const char *format, ...)
 {
@@ -26,9 +29,10 @@ int _printf(const char *format, ...)
 		{
 			if (format[i + 1] == 'c')
 			{
-				chars_printed += print_char(va_arg(args, char));
+				chars_printed += print_char(va_arg(args, int));
 				i++;
 			}
+
 			if (format[i + 1] == 's')
 			{
 				chars_printed += print_str(va_arg(args, char*));
@@ -39,15 +43,16 @@ int _printf(const char *format, ...)
 
 	va_end(args);
 
-	return(chars_printed);
+	return (chars_printed);
 }
 
-/*
-* print_char - function that prints a char.
-* Prototype: int print_char(char c);
-* @c: a character.
-* Returns: the number of characters printed.
-*/
+/**
+ * print_char - function that prints a char.
+ * Prototype: int print_char(char c);
+ * @c: a character.
+ * Return: the number of characters printed.
+ */
+
 int print_char(char c)
 {
 	_putchar(c);
@@ -56,12 +61,12 @@ int print_char(char c)
 }
 
 
-/*
-* print_str - function that prints a string of chars.
-* Prototype: int print_str(char *str);
-* @s: pointer to a string.
-* Returns: the number of characters printed.
-*/
+/**
+ * print_str - function that prints a string of chars.
+ * Prototype: int print_str(char *str);
+ * @str: pointer to a string.
+ * Return: the number of characters printed.
+ */
 
 int print_str(char *str)
 {
--- main.c
+++ main.c
@@ -9,13 +9,12 @@
  */
 int main(void)
 {
-
 	_printf("Let's try to printf a simple sentence.\n");
 	printf("Let's try to printf a simple sentence.\n");
 	_printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
 
-        return (0);
+	return (0);
 }
