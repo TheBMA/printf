5353d135712677b8c62c8bb5e88a0422cad322b8
TheBMA <benrochd.m.ali@gmail.com>
2023-12-04 22:48:33 +0100

    Revert last change

--- _printf.c
+++ _printf.c
@@ -78,17 +78,6 @@ int print_str(char *str)
 {
 	int i, chars_printed = 0;
 
-	if (str == NULL)
-	{
-		_putchar('(');
-		_putchar('n');
-		_putchar('u');
-		_putchar('l');
-		_putchar('l');
-		_putchar(')');
-		return (0);
-	}
-
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
